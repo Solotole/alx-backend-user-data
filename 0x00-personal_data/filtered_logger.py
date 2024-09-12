2603e0986ff0b0172930a02ba7f6eb253c920160
Solotole <solomontoleak47@gmail.com>
2024-09-12 10:22:16 +0100

    basic Regex-ing

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,7 +2,8 @@
 """Basic Regex-ing"""
 import re
 
+
 def filter_datum(fields, redaction, message, separator):
-  """Filters a log message by obfuscating specified fields."""
-  pattern = rf"(?:{separator})({'|'.join(fields)})=([^;]+)"
-  return re.sub(pattern, rf"\1={redaction}", message)
+    """Filters a log message by obfuscating specified fields."""
+    pattern = rf"(?:{separator})({'|'.join(fields)})=([^;]+)"
+    return re.sub(pattern, rf"\1={redaction}", message)
