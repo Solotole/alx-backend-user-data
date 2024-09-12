76f0cc2871ce422c5ef433ac11f0e25a8a5b8cdb
Solotole <solomontoleak47@gmail.com>
2024-09-12 12:07:34 +0100

    logging into log file

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Basic Regex-ing"""
 import re
+import logging
 from typing import List
 
 
@@ -13,3 +14,23 @@ def filter_datum(
     """Filters a log message by obfuscating specified fields."""
     pattern: str = rf"(?:{separator})({'|'.join(fields)})=([^;]+)"
     return re.sub(pattern, rf"\1={redaction}", message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """formating method"""
+        filter_string: str = filter_datum(self.fields, self.REDACTION, record.getMessage(), self.SEPARATOR)
+        # print(filter_string)
+        record.msg = filter_string
+        return super().format(record)
