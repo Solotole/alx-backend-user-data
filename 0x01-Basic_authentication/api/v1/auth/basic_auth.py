3c3c546709fe7b37dffefffc648db7a9d1167619
Solotole <solomontoleak47@gmail.com>
2024-09-18 00:24:03 +0100

    is it user or not?

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,8 @@
 from api.v1.auth.auth import Auth
 import base64
 import binascii
+from typing import TypeVar
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -55,3 +57,20 @@ class BasicAuth(Auth):
         first = decoded_base64_authorization_header[0:index]
         second = decoded_base64_authorization_header[index + 1:]
         return (first, second)
+
+    def user_object_from_credentials(
+        self, user_email: str, user_pwd: str
+    ) -> TypeVar('User'):
+        """returns the User instance based on his email and password
+        """
+        if user_email is None or type(user_email) is not str:
+            return None
+        if user_pwd is None or type(user_pwd) is not str:
+            return None
+        users = User.search({"email": user_email})
+        if not users or len(users) == 0:
+            return None
+        user = users[0]
+        if not user.is_valid_password(user_pwd):
+            return None
+        return user
