51faf0e2c4a0a8afd3d0a73b9621ac5c609e1a12
Solotole <solomontoleak47@gmail.com>
2024-09-17 23:00:57 +0100

    Basic - Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """another basic auth class"""
 from api.v1.auth.auth import Auth
+import base64, binascii
 
 
 class BasicAuth(Auth):
@@ -22,3 +23,17 @@ class BasicAuth(Auth):
                 authorization_header[5] != ' '):
             return None
         # return authorization_header[6:]
+    
+    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
+        """returns the decoded value of a Base64
+        string base64_authorization_header
+        """
+        if base64_authorization_header is None:
+            return None
+        if type(base64_authorization_header) is not str:
+            return None
+        try:
+            data = base64.b64decode(base64_authorization_header, validate=True)
+            return data.decode('utf-8')
+        except binascii.Error:
+            return None
