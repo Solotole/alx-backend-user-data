fc3160cbf9dd470ee3b8ed478d8258bf736e44a6
Solotole <solomontoleak47@gmail.com>
2024-09-17 21:15:28 +0100

    Request validation!

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -7,7 +7,7 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
-from typing import List, optional
+from typing import List, Optional
 
 
 app = Flask(__name__)
