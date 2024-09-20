4d81ef3510807d07d79f354e382ab1e211b39810
Solotole <solomontoleak47@gmail.com>
2024-09-20 02:19:33 +0100

    new path

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -35,11 +35,14 @@ def before_request_handler():
     excluded_paths: List[str] = [
         '/api/v1/status/',
         '/api/v1/unauthorized/',
-        '/api/v1/forbidden/'
+        '/api/v1/forbidden/',
+        '/api/v1/auth_session/login/'
     ]
-    request.current_user = auth.current_user(request)
     if not auth.require_auth(request.path, excluded_paths):
         return
+    if auth.authorization_header(request) is None and auth.session_cookie(request) is None:
+        abort(401)
+    request.current_user = auth.current_user(request)
     if auth.authorization_header(request) is None:
         abort(401)
     if auth.current_user(request) is None:
