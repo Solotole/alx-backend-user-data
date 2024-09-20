c6e844d02c509578fa47944d7b8246b74c9a8c6b
Solotole <solomontoleak47@gmail.com>
2024-09-20 03:52:52 +0100

    updated

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -30,27 +30,19 @@ else:
 def before_request_handler():
     """method to run before any request
     """
-    if auth is None:
-        return
-    excluded_paths: List[str] = [
-        '/api/v1/status/',
-        '/api/v1/unauthorized/',
-        '/api/v1/forbidden/',
-        '/api/v1/auth_session/login/'
-    ]
-    if not auth.require_auth(request.path, excluded_paths):
-        return
-    if (
-        auth.authorization_header(request) is None and
-        auth.session_cookie(request) is None
-    ):
-        abort(401)
-    request.current_user = auth.current_user(request)
-    if auth.authorization_header(request) is None:
-        abort(401)
-    if auth.current_user(request) is None:
-        abort(403)
+    authorized_list = ['/api/v1/status/',
+                       '/api/v1/unauthorized/', '/api/v1/forbidden/',
+                       '/api/v1/auth_session/login/']
 
+    if auth and auth.require_auth(request.path, authorized_list):
+        if not auth.authorization_header(request):
+            abort(401)
+        if (auth.authorization_header(request) and
+            not auth.session_cookie(request)):
+            abort(401)
+        request.current_user = auth.current_user(request)
+        if not auth.current_user(request):
+            abort(403)
 
 @app.errorhandler(404)
 def not_found(error) -> str:
