a9348bdc096bfc03c7a7e824f8a98f7539c7d92c
Solotole <solomontoleak47@gmail.com>
2024-09-27 17:33:10 +0100

    Generate reset password token

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -94,3 +94,12 @@ class Auth:
         except Exception:
             return None
         return None
+
+    def get_reset_password_token(self, email: str) -> str:
+        """Generate reset password token"""
+        user = self._db.find_user_by(email=email)
+        if not user:
+            raise ValueError
+        unique: str = _generate_uuid()
+        user['reset_token'] = unique
+        return unique
